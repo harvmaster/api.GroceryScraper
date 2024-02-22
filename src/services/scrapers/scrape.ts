93cb82dfc0ebc6d3fa3204e02f413d9a50137030
Harvmaster <hzuccon@gmail.com>
2024-02-22 17:17:33 +1100

    Complete rewrite of scrape.ts using Github Copilot

--- src/services/scrapers/scrape.ts
+++ src/services/scrapers/scrape.ts
@@ -7,20 +7,18 @@ import { getItemTags, getBulkItemTags } from '../ItemTagger';
 import getProductSimilarity from './getProductSimilarity';
 import matchProductWithGPT from './matchProductWithGPT';
 
-const scrape = async (scraper: Scraper) => {
-  // const existingProducts = await Products.find({});
-  const scrapedProducts = await scraper.scrapeAllCategories();
-  const existingProductListings = await ProductListings.find({})
+const getExistingProductListingIds = async () => {
+  const existingProductListings = await ProductListings.find({});
+  return existingProductListings.map((productListing) => productListing.supplier_product_id);
+}
 
-  const existingProductListingIds = existingProductListings.map((productListing) => productListing.supplier_product_id)
-  const initialProducts = scrapedProducts.filter((product) => !existingProductListingIds.includes(product.name))
+const getInitialProducts = (scrapedProducts, existingProductListingIds) => {
+  return scrapedProducts.filter((product) => !existingProductListingIds.includes(product.name));
+}
 
-  // Initial Products are products that are not in the database for that specific supplier.
-  // An initial product may have a product listing in the database for another supplier, in which case we need to link it. Otherwise we put a new Product in the database
-
-  // Get similar Products from the database
-  const existingProducts = await Products.find({})
-  const matches = initialProducts.map((initialProduct) => {
+const getMatches = async (initialProducts) => {
+  const existingProducts = await Products.find({});
+  return initialProducts.map((initialProduct) => {
     return {
       product: initialProduct,
       similarProducts: existingProducts.filter((existingProduct) => getProductSimilarity(initialProduct.name, existingProduct.name) > 0.8).map(product => {
@@ -30,37 +28,60 @@ const scrape = async (scraper: Scraper) => {
         }
       })
     }
-  })
+  });
+}
 
-  const productsWithSimilar = matches.filter((match) => match.similarProducts.length > 0)
-  const productsWithoutSimilar = matches.filter((match) => match.similarProducts.length === 0).map(product => product.product)
-
-  // If there are similar products, send them to chatGPT to decide what the best match is
-  const chatGPTMatchesPromises = productsWithSimilar.map(async (productWithSimilar) => matchProductWithGPT(productWithSimilar.product, productWithSimilar.similarProducts))
-  const chatGPTMatches = await Promise.all(chatGPTMatchesPromises)
-
-  // filter based on whether there was a match
-  const matchedProducts = chatGPTMatches.filter((match) => match.match)
-  productsWithoutSimilar.push(...chatGPTMatches.filter((match) => !match.match).map((match) => match.product))
-
-  // Products without a match are tagged and then added to the database
-  // Add tags to the new products
-  const tags = await getBulkItemTags(productsWithoutSimilar.map((product) => product.name))
-  const taggedProducts = Object.keys(tags).map((key) => {
-    const product = productsWithoutSimilar.find((product) => product.name === key)
-    if (product) {
-      product.tags = tags[key]
+const separateMatches = (matches) => {
+  const productsWithSimilar = [];
+  const productsWithoutSimilar = [];
+  matches.forEach((match) => {
+    if (match.similarProducts.length > 0) {
+      productsWithSimilar.push(match);
+    } else {
+      productsWithoutSimilar.push(match.product);
     }
-    return product
-  })
-  // Create the new products
-  const newProductPromises = taggedProducts.map(async (product) => {
-    const newProduct = new Products(product)
-    return newProduct.save()
-  })
-  const newProducts = await Promise.all(newProductPromises)
+  });
+  return { productsWithSimilar, productsWithoutSimilar };
+}
 
-  // Create the new product listings
+const getChatGPTMatches = async (productsWithSimilar) => {
+  const chatGPTMatchesPromises = productsWithSimilar.map(async (productWithSimilar) => matchProductWithGPT(productWithSimilar.product, productWithSimilar.similarProducts));
+  return await Promise.all(chatGPTMatchesPromises);
+}
+
+const separateChatGPTMatches = (chatGPTMatches) => {
+  const matchedProducts = [];
+  const unmatchedProducts = [];
+  chatGPTMatches.forEach((match) => {
+    if (match.match) {
+      matchedProducts.push(match);
+    } else {
+      unmatchedProducts.push(match.product);
+    }
+  });
+  return { matchedProducts, unmatchedProducts };
+}
+
+const getTaggedProducts = async (productsWithoutSimilar) => {
+  const tags = await getBulkItemTags(productsWithoutSimilar.map((product) => product.name));
+  return Object.keys(tags).map((key) => {
+    const product = productsWithoutSimilar.find((product) => product.name === key);
+    if (product) {
+      product.tags = tags[key];
+    }
+    return product;
+  });
+}
+
+const createNewProducts = async (taggedProducts) => {
+  const newProductPromises = taggedProducts.map(async (product) => {
+    const newProduct = new Products(product);
+    return newProduct.save();
+  });
+  return await Promise.all(newProductPromises);
+}
+
+const createNewProductListings = async (taggedProducts, scraper) => {
   const newProductListingPromises = taggedProducts.map(async (product) => {
     const newProductListing = new ProductListings({
       name: product.name,
@@ -68,15 +89,30 @@ const scrape = async (scraper: Scraper) => {
       supplier_product_id: product.supplier_product_id,
       supplier_name: scraper.name,
       tags: product.tags
-    })
-    return newProductListing.save()
-  })
-  const newProductListings = await Promise.all(newProductListingPromises)
-
-  // Create Product Price Event
-
+    });
+    return newProductListing.save();
+  });
+  return await Promise.all(newProductListingPromises);
 }
 
+const scrape = async (scraper: Scraper) => {
+  try {
+    const scrapedProducts = await scraper.scrapeAllCategories();
+    const existingProductListingIds = await getExistingProductListingIds();
+    const initialProducts = getInitialProducts(scrapedProducts, existingProductListingIds);
+    const matches = await getMatches(initialProducts);
+    const { productsWithSimilar, productsWithoutSimilar } = separateMatches(matches);
+    const chatGPTMatches = await getChatGPTMatches(productsWithSimilar);
+    const { matchedProducts, unmatchedProducts } = separateChatGPTMatches(chatGPTMatches);
+    productsWithoutSimilar.push(...unmatchedProducts);
+    const taggedProducts = await getTaggedProducts(productsWithoutSimilar);
+    const newProducts = await createNewProducts(taggedProducts);
+    const newProductListings = await createNewProductListings(taggedProducts, scraper);
+    // Create Product Price Event
+  } catch (error) {
+    console.error(`Failed to scrape: ${error}`);
+  }
+}
 
 const run = async () => {
   const colesScraper = new ColesScraper();
